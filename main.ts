f01d162b79c851ac54bf097de7252ac1abacc34b
Juri Adams <juri@4da.ms>
2020-04-30 11:46:34 +0200

    :zap: Added Auto Updater

--- main.ts
+++ main.ts
@@ -1,14 +1,33 @@
+/**
+ * Import Dependencies
+ */
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 import * as url from "url";
 
+/**
+ * Initiate Auto Updater
+ */
 // tslint:disable-next-line: no-var-requires
-require("update-electron-app")();
+require("update-electron-app")({
+    repo: "inexio/snmp-sim-ui",
+    updateInterval: "1 hour",
+});
 
+/**
+ * Create new BrowserWindow
+ */
 let win: BrowserWindow = null;
+
+/**
+ * Parse process args
+ */
 const args = process.argv.slice(1);
 const serve = args.some((val) => val === "--serve");
 
+/**
+ * Create the BrowserWindow and load Angular
+ */
 function createWindow(): BrowserWindow {
     // Create the browser window.
     win = new BrowserWindow({
@@ -37,15 +56,7 @@ function createWindow(): BrowserWindow {
         );
     }
 
-    if (serve) {
-        // win.webContents.openDevTools();
-    }
-
-    // Emitted when the window is closed.
     win.on("closed", () => {
-        // Dereference the window object, usually you would store window
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         win = null;
     });
 
@@ -53,23 +64,15 @@ function createWindow(): BrowserWindow {
 }
 
 try {
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
     app.on("ready", createWindow);
 
-    // Quit when all windows are closed.
     app.on("window-all-closed", () => {
-        // On OS X it is common for applications and their menu bar
-        // to stay active until the user quits explicitly with Cmd + Q
         if (process.platform !== "darwin") {
             app.quit();
         }
     });
 
     app.on("activate", () => {
-        // On OS X it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
         if (win === null) {
             createWindow();
         }
